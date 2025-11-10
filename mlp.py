371ba74ddd6c6faded38ba8a7aa1b8151870e783
o0Parzival0o <59389321+o0Parzival0o@users.noreply.github.com>
2025-11-10 21:42:39 +0100

    Bellurie

--- mlp.py
+++ mlp.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import numpy as np
+
 import json
 import random
+
+#=============================
+# Utility
+#=============================
 
 def load_config_json(filepath):
     """Loads the configuration from the specified JSON file."""
@@ -8,72 +14,88 @@ def load_config_json(filepath):
         config = json.load(f)
     return config
 
-
-class Neuron:
-    def __init__(self,bias):
-        self.bias = bias
-        self.weights = []
-    
-
-
-class NeuronLayer:
-    def __init__(self,neurons):
-        self.bias = random.random()
-        self.neurons = [Neuron(self.bias) for _ in range(neurons)]
-
-
-
-
-class NeuralNetwork:
-    def __init__(self,num_inputs,num_hidden,num_outputs,neurons_per_layer):
-        self.num_inputs = num_inputs
-        # self.hidden = NeuronLayer()#TODO valutare se ha senso implementare un diverso numero di neuroni per hidden layer
-        # self.num_outputs= num_outputs
-
-        self.hidden_layers = []
-
-        self.output_layer = NeuronLayer(num_outputs)
-
-
-        for neurons in neurons_per_layer:
-            self.hidden_layers.append(NeuronLayer(neurons))
-        def init_weights_from_inputs_to_hidden_layers(self):
-            weight_num = 0#TODO continuare da qui
-
-        
-
 def preprocess_monk():
+    """ Preprocess the MONK dataset by performing one-hot encoding on categorical features.  Returns the one-hot encoded patterns, targets and the number of input units."""
     data = pd.read_csv(config["paths"]["train_data"], sep=r"\s+", header=None)
-
     # print(data.head())
 
     # the first column in the dataset is the target, the remaining values are the pattern
-    #TODO vedere se c'è da rimuovere i nomi pure qua 
     targets = data[data.columns[0]]
-
     # print(targets.head(25))
 
-    #removing target and name
+    # removing target and name
     patterns = data.drop(data.columns[0], axis=1)
-    patterns = patterns.drop(data.columns[7], axis=1)#TODO capire come adattare esigenze specifiche del dataset ad altri casi generali (o se fare due versioni separate per dataset)
+    patterns = patterns.drop(data.columns[-1], axis=1)
     # print(patterns.head(5))
 
-    #this is necessary for pandas to get the dummies otherwise it isn't happy
+    # this is necessary for pandas to get the dummies otherwise it isn't happy
     patterns = patterns.astype(str)
 
-
     one_hot_encoding = pd.get_dummies(patterns, prefix=['A1','A2','A3','A4','A5','A6'])
-
     # print(one_hot_encoding)
 
     input_units_number = one_hot_encoding.shape[1]
     return one_hot_encoding,targets,input_units_number
 
+def preprocess_exam_file():
+    """ Preprocess the exam dataset.  Returns the targets and the number of input units."""
+    data = pd.read_csv(config["paths"]["exam_file"], sep=r"\s+", header=None)
+
+    # the lasts 4 columns in the dataset are the targets, the remaining values are the pattern and examples index
+    targets = data[data.columns[-4:]]
+    # print(targets.head(25))
+
+    #removing target and name
+    patterns = data.drop(data.columns[-4:], axis=1)
+    patterns = patterns.drop(data.columns[0], axis=1)
+    # print(patterns.head(5))
+
+    input_units_number = patterns.shape
+    return targets,input_units_number
 
 
-config = load_config_json("config.json")
-one_hot_encoding,targets,input_units_number = preprocess_monk()
+#=============================
+# Neural Network
+#=============================
+
+class Neuron:
+    """ Represents a single neuron in the neural network. """
+    def __init__(self, bias):
+        self.bias = bias
+        self.weights = []
+    
+class NeuronLayer:
+    """ Represents a layer of neurons in the neural network. """
+    def __init__(self, neurons):
+        self.bias = random.random()
+        self.neurons = [Neuron(self.bias) for _ in range(neurons)]
+
+class NeuralNetwork:
+    """ Represents a multi-layer perceptron neural network. """
+    def __init__(self, num_inputs, num_hidden, num_outputs, neurons_per_layer):
+        self.num_inputs = num_inputs
+        # self.hidden = NeuronLayer()                       # TODO valutare se ha senso implementare un diverso numero di neuroni per hidden layer
+        # self.num_outputs= num_outputs
+
+        self.hidden_layers = []
+        self.output_layer = NeuronLayer(num_outputs)
+
+        for neurons in neurons_per_layer:
+            self.hidden_layers.append(NeuronLayer(neurons))
+
+        def init_weights_from_inputs_to_hidden_layers(self):
+            weight_num = 0                                  #TODO continuare da qui
+
+        
 
 
+
+
+if __name__ == "__main__":
+
+    config = load_config_json("config.json")
+    one_hot_encoding,targets,input_units_number = preprocess_monk()
+
+    # one_hot_encoding,targets,input_units_number = preprocess_exam_file()
 
 
