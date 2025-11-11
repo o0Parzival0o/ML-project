ce879fa3f64e2b4a857134327b06e40ecc4429a6
amarcheschi <157390625+amarcheschi@users.noreply.github.com>
2025-11-11 16:05:30 +0100

    Update mlp.py
    added weight initialization

--- mlp.py
+++ mlp.py
@@ -50,8 +50,16 @@ def preprocess_exam_file():
     patterns = patterns.drop(data.columns[0], axis=1)
     # print(patterns.head(5))
 
-    input_units_number = patterns.shape[1]
+    input_units_number = patterns.shape
     return targets,input_units_number
+
+#can choose between different extraction methods for the weights/biases
+def create_random_extractor(method):
+    if(method == "standard"):
+        def extractor_function():
+            return random.uniform(-0.7,0.7)
+        return extractor_function
+
 
 
 #=============================
@@ -60,42 +68,76 @@ def preprocess_exam_file():
 
 class Neuron:
     """ Represents a single neuron in the neural network. """
-    def __init__(self):
-        self.bias = random.uniform(-1, 1)
+    def __init__(self, bias):
+        self.bias = bias
         self.weights = []
     
 class NeuronLayer:
     """ Represents a layer of neurons in the neural network. """
-    def __init__(self, num_neurons, num_inputs_per_neuron):
-        self.neurons = []
-        for _ in range(num_neurons):
-            neuron = Neuron()
-            neuron.weights = [random.uniform(-1, 1) for _ in range(num_inputs_per_neuron)]
-            self.neurons.append(neuron)
+    def __init__(self, neurons):
+        self.bias = extractor()
+        self.neurons = [Neuron(self.bias) for _ in range(neurons)]
+        self.num_neurons = neurons
 
 class NeuralNetwork:
     """ Represents a multi-layer perceptron neural network. """
-    def __init__(self, num_inputs,  neurons_per_layer, num_outputs):
-        # hidden layers
-        input_size = num_inputs
+    def __init__(self, num_inputs, num_outputs, neurons_per_layer):
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+
+
+        self.neurons_per_layer = neurons_per_layer #neurons per HIDDEN layer
+        self.hidden_layers_number = len(neurons_per_layer) #how many hidden layers
         self.hidden_layers = []
+        self.output_layer = []#TODO più in la col training valutare se ha senso accorpare hidden layers con input e output in un unica struttura layers
 
-        for neurons in neurons_per_layer:
-            layer = NeuronLayer(neurons, input_size)
+
+
+        #initialize hidden weights in a loop
+        for i in range(self.hidden_layers_number):
+            num_neurons = self.neurons_per_layer[i]
+            #only hidden weights number can be chosen
+            num_inputs = self.num_inputs if i == 0 else self.neurons_per_layer[i-1] 
+
+            layer = NeuronLayer(num_neurons)
+
+            self.init_weights(layer,num_inputs)
             self.hidden_layers.append(layer)
-            input_size = neurons
-        
-        # output layer
-        self.output_layer = NeuronLayer(num_outputs, input_size)
+
+        output_layer = NeuronLayer(num_outputs)
+        self.init_weights(output_layer, self.hidden_layers[-1].num_neurons)
+        self.output_layer = output_layer
+
+    def init_weights(self,layer,num_prev_inputs):
+        # weight_num = 0
+        for neuron in layer.neurons:
+            for i in range(num_prev_inputs):
+                neuron.weights.append(extractor())
+        # weight_num = 0
+        # for neuron in 
+    def print_network(self):
+        print(f'the net has {self.num_inputs} input neurons')
+        print(f'the net has {self.neurons_per_layer} hidden layers')
+        for i,layer in enumerate(self.hidden_layers):
+            print(f'hidden layer {i} weights:')
+            for neuron in layer.neurons:
+                print(neuron.weights) 
+        print(f'the net has {self.num_outputs} input neurons')
+        print(f'weights connecting last hidden layer to output:')
+        for neuron in self.output_layer.neurons:
+            print(neuron.weights)
 
 
-        def init_weights_from_inputs_to_hidden_layers(self):
-            weight_num = 0                                  #TODO continuare da qui
-        
+    
+
+
+            
+
+
 
 
 #=============================
-# Activation & Loss Functions
+# Activation Functions
 #=============================
 
 def sigmoid(x, a):
@@ -107,22 +149,32 @@ def tanh(x, a):
 def relu(x):
     return np.maximum(0, x)
 
-def softplus(x):
-    return np.log1p(np.exp(-np.abs(x))) + np.maximum(x, 0)
+def softmax(x):
+    return np.log(1 + np.e**x)
 
 def tanh_like(x, n):
     return np.sign(x) * (1 + ((2**n * np.abs(x) - np.floor(2**n * np.abs(x)))/2 - 1)/(2**(np.floor(2**n * np.abs(x)))))
 
-def MSE(o, t):
-    l = t.shape[0]
-    return 1/l * np.sum(np.sum((o-t)**2, axis=1), axis=0)
 
-def MEE(o, t):
-    l = t.shape[0]
-    return 1/l * np.sum(np.sqrt(np.sum((o-t)**2, axis=1)), axis=0)
+
+#=============================
+# Initialization Flags
+#=============================
+
+
+
 
 if __name__ == "__main__":
 
     config = load_config_json("config.json")
-    one_hot_encoding,targets,input_units_number = preprocess_monk()    
+    one_hot_encoding,targets,input_units_number = preprocess_monk()
+    # print(input_units_number,config["architecture"]["output_units"],config["architecture"]["neurons_per_layer"])
+    extractor = create_random_extractor(config["initialization"]["method"])
+
+    mlp = NeuralNetwork(input_units_number,config["architecture"]["output_units"],config["architecture"]["neurons_per_layer"])
+
+
+    mlp.print_network()
     # one_hot_encoding,targets,input_units_number = preprocess_exam_file()
+
+
