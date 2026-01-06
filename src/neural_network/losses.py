77fd498e2193edd0ce8fbed3d38665bb38e25317
Parzival <lucanas2003@gmail.com>
2026-01-06 11:24:03 +0100

    fine (?)

--- src/neural_network/losses.py
+++ src/neural_network/losses.py
@@ -2,21 +2,13 @@ import numpy as np
 
 
 def MSE(o, t):
-    t = t.astype(float)
-
-    eps = 1e-15
-    o_clip = np.clip(o, eps, 1-eps)
-    
-    diff = o_clip - t
+    t = t.astype(float)    
+    diff = o - t
     return np.mean(diff**2)
 
 def MEE(o, t):
     t = t.astype(float)
-    
-    eps = 1e-15
-    o_clip = np.clip(o, eps, 1-eps)
-    
-    diff = o_clip - t
+    diff = o - t
     return np.mean(np.sqrt(np.sum(diff**2, axis=1)))
 
 def binary_crossentropy(o, t):
